e84c61d174dc755ac3a3d6ad4f440df1efa52455
Franz <franzcmarcelo@gmail.com>
2022-11-06 01:55:26 -0500

    [redux-toolkit] add reducer from slice to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { counterSlice } from './slices/counter'
 
 export const store = configureStore({
   reducer: {
-    counter: {},
+    counter: counterSlice.reducer,
   },
 })
